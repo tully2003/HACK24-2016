97d8ab19b14ad03e161ced72a825ebad025abc0d
Stuart Bennett <stuart.bennett@live.co.uk>
2016-03-19 17:27:19 +0000

    working sample of Chrome app detecting two monitors and opening two windows1

---
+++ chrome-app/go.bat
@@ -0,0 +1,1 @@
+chrome --load-and-launch-app=%USERPROFILE%\HACK24-2016\chrome-app\src
---
+++ chrome-app/src/background.js
@@ -0,0 +1,24 @@
+var getInfoDone = function(result) {
+
+};
+
+chrome.app.runtime.onLaunched.addListener(function() {
+
+   chrome.system.display.getInfo(function(displays) {
+
+	var display = displays[1];
+    
+	chrome.app.window.create('window.html', {
+	    'outerBounds': {
+		'left': display.bounds.left,
+		'top': 0 
+	    },
+	    'state': 'fullscreen'
+	});
+
+	chrome.app.window.create('window-2.html', {
+	    'state': 'fullscreen'
+	});
+	
+   }); 
+});
