8380b0fdc0d5bc6daf73ab9813ef0620f763a1ff
Alex Tully <alex.tully@bizfitech.com>
2016-03-19 21:13:51 +0000

    signalr method sigs

--- Hack24/Hubs/GameHub.cs
+++ Hack24/Hubs/GameHub.cs
@@ -4,6 +4,42 @@
 
     public class GameHub : Hub
     {
+        public void CreateGame()
+        {
+        }
+
+        public void JoinGame()
+        {
+        }
+
+        public void LeaveGame()
+        {
+        }
+
+        public void PlayerReady()
+        {
+        }
+
+        public void StartGame()
+        {
+        }
+
+        public void PlayerMove()
+        {
+        }
+
+        public void CollectPiece()
+        {
+        }
+
+        public void PlacePiece()
+        {
+        }
+
+        public void GameWon()
+        {
+        }
+
         public void Send(string player)
         {
             Clients.All.playerJoined(player);
