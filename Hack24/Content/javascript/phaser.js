3bfc28f8e926aa1212a3a16c1c92de3fa43d3624
Chris Rickels <crickels@gmail.com>
2016-03-20 05:44:12 +0000

    Changed graphics and map

--- Hack24/Content/javascript/phaser.js
+++ Hack24/Content/javascript/phaser.js
@@ -3,15 +3,17 @@
 		var gameWidth = $(window).width();
       var gameHeight = gameWidth * 0.56;
       
-		var game = new Phaser.Game(800, 576, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+		var game = new Phaser.Game(2560, 1280, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
     game.load.tilemap('map', '/Content/assets/map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', '/content/assets/gridtiles.png');
-    game.load.spritesheet('player', '/content/assets/spaceman.png', 16, 16);
-    game.load.spritesheet('coin', '/content/assets/coin.png', 32, 32);
+    //game.load.image('tiles', '/content/assets/tiles_hack24.png');
+    game.load.image('tiles', '/content/assets/council_tiles.png');
+    game.load.spritesheet('player', '/content/assets/spaceman.png', 128, 128);
+    //game.load.spritesheet('coin', '/content/assets/coin.png', 32, 32);
     //game.load.spritesheet('puzzle_piece', '/content/assets/puzzle_piece.png', 32, 32);
+    game.load.image('background', '/content/assets/background.png');
 
 }
 var coins;
@@ -22,10 +24,16 @@ var player;
 var pieces;
 var puzzle_pieces;
 
-function create() {
+var player_speed = 300;
 
-   game.stage.backgroundColor = '#787878';
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#787878';
+    game.stage.backgroundImage = 
    
+    game.add.sprite(0, 0, 'background');
+
    sizeGameToWindow();
 
    //game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -37,29 +45,29 @@ function create() {
     map = game.add.tilemap('map');
 
     //  Now add in the tileset
-    map.addTilesetImage('walls', 'tiles');
+    //map.addTilesetImage('walls', 'tiles');
+    map.addTilesetImage('council', 'tiles');
     
     //  Create our layer
-    layer = map.createLayer('Tile Layer 1');
+    layer = map.createLayer('Collision');
 
     //layerObj = map.createLayer('Object Layer 1');
     
 
-    map.setCollision([90,99]);
+    map.setCollision([52]);
     //  Resize the world
     layer.resizeWorld();
 
     //  Un-comment this on to see the collision tiles
-    //layer.debug = true;
+    layer.debug = true;
 
-    coins = game.add.group();
-    //puzzle_pieces = game.add.group();
+    //coins = game.add.group();
 
-    coins.enableBody = true;
+    //coins.enableBody = true;
 
-    map.createFromObjects('Object Layer 1', 90, 'coin', 0, true, false, coins);
-    coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
-    coins.callAll('animations.play', 'animations', 'spin');
+    //map.createFromObjects('Object Layer 1', 20, 'coin', 0, true, false, coins);
+    //coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
+    //coins.callAll('animations.play', 'animations', 'spin');
 
     //if (coins.children.length > 0) {
     //    for (var i = 0; i < coins.children.length; i++) {
@@ -69,18 +77,20 @@ function create() {
     //    //game.add.sprite(coins.children[1].world.x, coins.children[0].world.y, 'player', 1);
     //}
 
-    //debugger
 
     //  Player
-    player = game.add.sprite(48, 48, 'player', 1);
+    player = game.add.sprite(100, 100, 'player', 1);
     player.animations.add('left', [8,9], 10, true);
     player.animations.add('right', [1,2], 10, true);
     player.animations.add('up', [11,12,13], 10, true);
     player.animations.add('down', [4,5,6], 10, true);
 
     game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.collideWorldBounds = true;
 
-    player.body.setSize(10, 14, 2, 1);
+    player.body.setSize(128, 128, 0, 0);
+
+    //player.scale.setTo(2, 2);
 
     //game.camera.follow(player);
 
@@ -105,21 +115,24 @@ function gofull() {
 function update() {
 
     game.physics.arcade.collide(player, layer);
-    game.physics.arcade.overlap(player, coins, collectCoin, null, this);
+    //game.physics.arcade.overlap(player, coins, collectCoin, null, this);
 
     player.body.velocity.set(0);
 
     if (cursors.left.isDown)
     {
+        
         movePlayer(player, 'left');
     }
     else if (cursors.right.isDown)
     {
-        movePlayer(player,'right');
+        movePlayer(player, 'right');
     }
     else
     {
-        player.animations.stop();
+        player.animations.stop('left');
+        player.animations.stop('right');
+        //player.animations.stop();
     }
     
     if (cursors.up.isDown)
@@ -132,7 +145,9 @@ function update() {
     }
     else
     {
-        player.animations.stop();
+        player.animations.stop('up');
+        player.animations.stop('down');
+        //player.animations.stop();
     }
 
 }
@@ -140,19 +155,19 @@ function update() {
 function movePlayer(playerObj, direction) {
     switch (direction) {
         case 'up':
-            playerObj.body.velocity.y = -100;
+            playerObj.body.velocity.y = -player_speed;
             playerObj.play('up');
             break;
         case 'down':
-            playerObj.body.velocity.y = 100;
+            playerObj.body.velocity.y = player_speed;
             playerObj.play('down');
             break;
         case 'left':
-            playerObj.body.velocity.x = -100;
+            playerObj.body.velocity.x = -player_speed;
             playerObj.play('left');
             break;
         case 'right':
-            playerObj.body.velocity.x = 100;
+            playerObj.body.velocity.x = player_speed;
             playerObj.play('right');
             break;
     }
@@ -166,7 +181,7 @@ function collectCoin(player, coin) {
 
 function render() {
 
-    //game.debug.body(player);
+    game.debug.body(player);
 
 }
 
