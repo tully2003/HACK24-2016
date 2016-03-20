18bcedb229b641771561142217ca255b8c2fb903
Alex Tully <alex.tully@bizfitech.com>
2016-03-20 11:36:26 +0000

    clean up of csproj

--- Hack24/Content/js/game/main.js
+++
@@ -1,8 +0,0 @@
-var Packed24 = Packed24 || {};
-
-Packed24.game = new Phaser.Game(2560, 1280, Phaser.CANVAS, Phaser.AUTO, 'Packed 24');
- 
-Packed24.game.state.add('Boot', Packed24.Boot);
-Packed24.game.state.add('Preload', Packed24.Preload);
-Packed24.game.state.add('MainMenu', Packed24.MainMenu);
-Packed24.game.state.add('Game', Packed24.Game);
